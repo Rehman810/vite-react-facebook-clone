7f390adbef810c3e90af3e1ddcad558b5fee0a3d
Abdul Rehman <rehmankp186@gmail.com>
2024-01-06 00:25:29 +0500

    Add skeleton loading in TextPost

--- src/components/MainContent/HomePageComp/Skeleton.jsx
+++ src/components/MainContent/HomePageComp/Skeleton.jsx
@@ -4,35 +4,36 @@ import { RxCross2 } from "react-icons/rx";
 import { AiTwotoneLike } from "react-icons/ai";
 import { FaRegComment } from "react-icons/fa";
 import { AiOutlineLike } from "react-icons/ai";
+import { Skeleton, Space } from "antd";
+import { PiShareFatThin } from "react-icons/pi";
 
-import { Skeleton } from "antd";
-
-const Skeleton = () => {
+const SkeletonPage = () => {
   return (
     <>
       <div className="Post">
         <div className="post-head">
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-            }}
-          >
-            <Skeleton.Avatar />
+          <Space>
             <div
               style={{
                 display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start",
+                flexDirection: "row",
+                alignItems: "center",
               }}
             >
-              <span style={{ marginTop: "5px", fontSize: 18 }}>
-                <Skeleton active />
-              </span>
-              <p style={{ fontSize: 11, marginTop: "-1px" }}>{a.date}</p>
+              <Skeleton.Avatar />
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                }}
+              >
+                <span style={{ marginLeft: "10px", fontSize: 18 }}>
+                  <Skeleton.Input active size={"default"} />
+                </span>
+              </div>
             </div>
-          </div>
+          </Space>
           <div>
             <BiDotsHorizontalRounded className="post-head-icon" size={20} />
             <RxCross2 className="post-head-icon" size={20} />
@@ -73,4 +74,4 @@ const Skeleton = () => {
   );
 };
 
-export default Skeleton;
+export default SkeletonPage;
--- src/components/MainContent/HomePageComp/TextPost.jsx
+++ src/components/MainContent/HomePageComp/TextPost.jsx
@@ -1,25 +1,17 @@
 import React, { useContext, useState, useEffect } from "react";
-import { BiDotsHorizontalRounded } from "react-icons/bi";
 import { RxCross2 } from "react-icons/rx";
 import Profile from "../../../assets/blank-profile.png";
-import Posts from "../../../assets/story.png";
 import { AiTwotoneLike } from "react-icons/ai";
 import { FaRegComment } from "react-icons/fa";
 import { AiOutlineLike } from "react-icons/ai";
 import { PiShareFatThin } from "react-icons/pi";
-import { UserDataContext } from "../../../Context/Context";
-import {
-  collection,
-  query,
-  orderBy,
-  onSnapshot,
-  getDocs,
-} from "firebase/firestore";
-import { auth, db } from "../../../firebase";
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
+import { db } from "../../../firebase";
+import SkeletonPage from "./Skeleton";
 
 const TextPost = () => {
-  const { userData } = useContext(UserDataContext);
   const [userPosts, setUserPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchAllUsersPosts = () => {
@@ -61,6 +53,7 @@ const TextPost = () => {
 
                   // Update state with the sorted posts from all users
                   setUserPosts(allUsersPostsData);
+                  setLoading(false);
                 }
               );
             });
@@ -80,94 +73,100 @@ const TextPost = () => {
 
   return (
     <>
-      {userPosts.map((a, index) => (
-        <div className="Post" key={index}>
-          <div className="post-head">
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-              }}
-            >
-              {a.photoURL ? (
-                <img
-                  style={{
-                    width: 40,
-                    height: 40,
-                    borderRadius: "50px",
-                    marginLeft: 10,
-                    marginRight: 20,
-                  }}
-                  src={a.photoURL}
-                  alt="profile"
-                />
-              ) : (
-                <img
-                  style={{
-                    width: 40,
-                    height: 40,
-                    borderRadius: "50px",
-                    marginLeft: 10,
-                    marginRight: 20,
-                  }}
-                  src={Profile}
-                  alt="profile"
-                />
-              )}
+      {loading ? (
+        <SkeletonPage />
+      ) : (
+        userPosts.map((a, index) => (
+          <div className="Post" key={index}>
+            <div className="post-head">
               <div
                 style={{
                   display: "flex",
-                  flexDirection: "column",
-                  alignItems: "flex-start",
+                  flexDirection: "row",
+                  alignItems: "center",
                 }}
               >
-                <span style={{ marginTop: "5px", fontSize: 18 }}>{a.name}</span>
-                <p style={{ fontSize: 11, marginTop: "-1px" }}>{a.date}</p>
+                {a.photoURL ? (
+                  <img
+                    style={{
+                      width: 40,
+                      height: 40,
+                      borderRadius: "50px",
+                      marginLeft: 10,
+                      marginRight: 20,
+                    }}
+                    src={a.photoURL}
+                    alt="profile"
+                  />
+                ) : (
+                  <img
+                    style={{
+                      width: 40,
+                      height: 40,
+                      borderRadius: "50px",
+                      marginLeft: 10,
+                      marginRight: 20,
+                    }}
+                    src={Profile}
+                    alt="profile"
+                  />
+                )}
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "flex-start",
+                  }}
+                >
+                  <span style={{ marginTop: "5px", fontSize: 18 }}>
+                    {a.name}
+                  </span>
+                  <p style={{ fontSize: 11, marginTop: "-1px" }}>{a.date}</p>
+                </div>
+              </div>
+              <div>
+                {/* <BiDotsHorizontalRounded className="post-head-icon" size={20} /> */}
+                <RxCross2 className="post-head-icon" size={20} />
               </div>
             </div>
-            <div>
-              {/* <BiDotsHorizontalRounded className="post-head-icon" size={20} /> */}
-              <RxCross2 className="post-head-icon" size={20} />
+            <p>{a.text}</p>
+            {/* {console.log(a.postURL)} */}
+            {a.postURL ? (
+              <div>
+                <img
+                  src={`${a.postURL}?cache=${Math.random()}`}
+                  alt="post"
+                  className="post-img"
+                />
+              </div>
+            ) : null}
+            <div className="like-comment">
+              <div>
+                <AiTwotoneLike size={20} color="blue" />
+                <span>1k likes</span>
+              </div>
+              <div>
+                <span>1.5k comments</span> &nbsp;&nbsp;
+                <span>500 shares</span>
+              </div>
+            </div>
+            <div className="post-btn">
+              <div className="like-btn">
+                <AiOutlineLike size={20} />
+                <span>Like</span>
+              </div>
+              <div className="like-btn">
+                <FaRegComment size={20} />
+                <span>Comment</span>
+              </div>
+              <div className="like-btn">
+                <PiShareFatThin size={20} />
+                <span>Share</span>
+              </div>
             </div>
           </div>
-          <p>{a.text}</p>
-          {/* {console.log(a.postURL)} */}
-          {a.postURL ? (
-            <div>
-              <img
-                src={`${a.postURL}?cache=${Math.random()}`}
-                alt="post"
-                className="post-img"
-              />
-            </div>
-          ) : null}
-          <div className="like-comment">
-            <div>
-              <AiTwotoneLike size={20} color="blue" />
-              <span>1k likes</span>
-            </div>
-            <div>
-              <span>1.5k comments</span> &nbsp;&nbsp;
-              <span>500 shares</span>
-            </div>
-          </div>
-          <div className="post-btn">
-            <div className="like-btn">
-              <AiOutlineLike size={20} />
-              <span>Like</span>
-            </div>
-            <div className="like-btn">
-              <FaRegComment size={20} />
-              <span>Comment</span>
-            </div>
-            <div className="like-btn">
-              <PiShareFatThin size={20} />
-              <span>Share</span>
-            </div>
-          </div>
-        </div>
-      ))}
+        ))
+      )}
     </>
   );
 };
